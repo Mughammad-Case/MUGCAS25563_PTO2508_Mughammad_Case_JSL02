012e2b661f4c95c4afe2cc8f8719d00d6aac1427
Mughammad <caseameen44@gmail.com>
2026-02-15 20:59:45 +0200

    Prompt user for task 1 title and description

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-// Testing if script file is linked correctly
-
-confirm("script.js is linked successfully.");
+// Prompt user for task 1 title and description
+const task1Title = prompt("Enter task 1 title:");
+const task1Description = prompt("Enter task 1 description:");
