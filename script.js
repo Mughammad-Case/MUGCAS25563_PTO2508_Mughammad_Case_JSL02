0004668f5795585da3659f0c7c83e767d58e954f
Mughammad <caseameen44@gmail.com>
2026-02-15 21:05:14 +0200

    Prompt user for task 2 title and description

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 // Prompt user for task 1 title and description
 const task1Title = prompt("Enter task 1 title:");
 const task1Description = prompt("Enter task 1 description:");
+
+// Prompt user for task 2 title and description
+const task2Title = prompt("Enter task 2 title:");
+const task2Description = prompt("Enter task 2 description:");
